76f5d61ec9ff4e197b72f4ed3ad14e3803519320
unknown <vovanty0504@gmail.comgit config --global user.name vovanty0504>
2022-10-17 13:51:34 +0700

    a

---
+++ 03_angular/execrise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  firstNumber: number;
+  secondNumber: number;
+  result: any;
+
+  constructor() {
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  resultSum(operator: string) {
+    switch (operator) {
+      case'+' :
+        return this.result = this.firstNumber + this.secondNumber;
+      case '-':
+        return this.result = this.firstNumber - this.secondNumber;
+      case '*':
+        return this.result = this.firstNumber * this.secondNumber;
+      case '/':
+        if (this.secondNumber === 0) {
+          return this.result = 'không được phép chia 0';
+        }
+        return this.result = this.firstNumber / this.secondNumber;
+    }
+  }
+}
